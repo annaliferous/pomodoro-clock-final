98a77313b2ff0b5bbaf4523eff1c06ec6cd58303
annaliferous <50800262+annaliferous@users.noreply.github.com>
2023-03-15 14:19:04 +0100

    skills

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<App 
   githubURL="https://github.com/completejavascript/pomodoro-clock" 
   defaultBreakLength='5' 
-  defaultSessionLength='20' />, 
+  defaultSessionLength='35' />, 
   document.getElementById('root')
 );
 
