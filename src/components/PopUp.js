65e22d2f2ce6c0915ab502a872bab087ffc098c8
annaliferous <50800262+annaliferous@users.noreply.github.com>
2023-03-16 17:29:29 +0100

    m

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -17,18 +17,18 @@ export default class PopUp extends Component{
                     
                     <table >
                         <tr>
-                        <td><button className='button' id='btn1' onClick={(e) => {e.preventDefault(); window.open('Skills/Breathe/breathe.html');}}>Breathe <br></br><font size="3">Breathe energy in. Hold. Breathe stress out.</font></button></td>
+                        <td><button className='button' id='btn1' onClick={(e) => {e.preventDefault(); window.open('https://charlesstover.github.io/meditative-breathing/');}}>Breathe <br></br><font size="3">Breathe energy in. Hold. Breathe stress out.</font></button></td>
                         <td><button className='button' id='btn2' onClick={(e) => {e.preventDefault(); window.open('/Skills/js-expanding-cards/index.html');}}> Zen Cards <br></br><font size="3">A couple adorble exercises for every time of the day!</font></button></td>
                         <td><button className='button' id='btn3' onClick={(e) => {e.preventDefault(); window.open('Skills/memory-game/memory-game.html');}}> Memory <br></br><font size="3">You are never to old for memory!</font> </button></td>
                         </tr>
                         <tr>
-                        <td><button className='button' id='btn4' onClick={(e) => {e.preventDefault(); window.open('Skills/circles/circles.html');}}> Circles <br></br><font size="3">Create circles and watch them wander.</font></button></td>
+                        <td><button className='button' id='btn4' onClick={(e) => {e.preventDefault(); window.open('https://plextora.itch.io/circles');}}> Circles <br></br><font size="3">Create circles and watch them wander.</font></button></td>
                         <td><button className='button' id='btn6' onClick={(e) => {e.preventDefault(); window.open('https://susam.net/invaders.html');}}> Space Invaders <br></br><font size="3">You know it! You love it!<br></br> Enjoy it!</font></button></td>
-                        <td><button className='button' id='btn6' onClick={(e) => {e.preventDefault(); window.open('Skills/jumping_frogs/index.html');}}> Jumping Frogs <br></br><font size="3">Make these adorble frogs jump to solve the puzzle!</font></button></td>
+                        <td><button className='button' id='btn6' onClick={(e) => {e.preventDefault(); window.open('https://mikeleshtembari.github.io/jumping-frogs/');}}> Jumping Frogs <br></br><font size="3">Make these adorble frogs jump to solve the puzzle!</font></button></td>
                         </tr>
                         <tr>
-                        <td><button className='button' id='btn7' onClick={(e) => {e.preventDefault(); window.open('Skills/tether/index.html');}}> Thether <br></br><font size="3">You need to let out some energy? Look no further!</font></button></td>
-                        <td><button className='button' id='btn8' onClick={(e) => {e.preventDefault(); window.open('Skills/soothing-sounds/index.html');}}> Soothing sounds <br></br><font size="3">A lovely selection of relaxing soundbits.</font></button></td>
+                        <td><button className='button' id='btn7' onClick={(e) => {e.preventDefault(); window.open('https://tether-game.web.app/');}}> Thether <br></br><font size="3">You need to let out some energy? Look no further!</font></button></td>
+                        <td><button className='button' id='btn8' onClick={(e) => {e.preventDefault(); window.open('https://rohan-kiratsata.github.io/soothing-sounds/');}}> Soothing sounds <br></br><font size="3">A lovely selection of relaxing soundbits.</font></button></td>
                         <td><button className='button disabled' id='btn9' on><i className="fa fa-plus-circle"></i></button></td>
                         </tr>
 
